2da8d60ab513a291905d9b7d2087fce47050c6bb
NIkolay Koev <nikslotar@abv.bg>
2022-07-28 22:46:29 +0300

    Online Books Library

---
+++ JS Applications Exam - 8 Aug 2021/src/app.js
@@ -0,0 +1,57 @@
+import { logout } from './api/users.js';
+import { render, page } from './lib.js';
+import { getUserData } from './util.js';
+import { dashboardView } from './views/dashboard.js';
+import { loginView } from './views/login.js';
+import { registerView } from './views/register.js';
+import { createView } from './views/create.js';
+import { detailsView } from './views/details.js';
+import { editView } from './views/edit.js';
+import { profileView } from './views/profile.js';
+
+
+document.getElementById('logoutBtn').addEventListener('click', onLogout);
+const main = document.querySelector('main');
+
+page(decorateContext);
+page('/', dashboardView);
+page('/books/:id', detailsView);
+page('/edit/:id', editView);
+page('/login', loginView);
+page('/register', registerView);
+page('/create', createView);
+page('/profile', profileView);
+
+// Start Application
+updateNav();
+page.start();
+
+function decorateContext(ctx, next) {
+    ctx.render = renderMain;
+    ctx.updateNav = updateNav;
+
+    next();
+}
+
+function renderMain(templateResult) {
+    render(templateResult, main);
+}
+
+function updateNav() {
+    const userData = getUserData();
+
+    if (userData) {
+        document.querySelector('#user').style.display = 'block';
+        document.querySelector('#guest').style.display = 'none';
+        document.querySelector('#user span').textContent = `Welcome, ${userData.email}`;
+    } else {
+        document.querySelector('#user').style.display = 'none';
+        document.querySelector('#guest').style.display = 'block';
+    }
+}
+
+function onLogout() {
+    logout();
+    updateNav();
+    page.redirect('/');
+}
