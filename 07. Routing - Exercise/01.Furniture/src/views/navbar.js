c873dfd45f7dc6fd81f49e186a5e12d87a0e1caa
NIkolay Koev <nikslotar@abv.bg>
2022-07-30 00:58:37 +0300

    Add Login

---
+++ 07. Routing - Exercise/01.Furniture/src/views/navbar.js
@@ -0,0 +1,31 @@
+import { logout } from "../api/users.js";
+import { html, render } from "../lib.js";
+import { getUserData } from "../util.js";
+
+const nav = document.querySelector('nav');
+
+const navbarTemplate = (isUser) => html`
+    ${isUser ? html`<a id="catalogLink" href="index.html">Dashboard</a>
+    <div id="user">
+        <a id="createLink" href="/create">Create Furniture</a>
+        <a id="profileLink" href="/my-furniture">My Publications</a>
+        <a @click=${onLogout} id="logoutBtn" href="javascript:void(0)">Logout</a>
+    </div>`
+    : html`<a id="catalogLink" href="index">Dashboard</a>
+    <div id="guest">
+        <a id="loginLink" href="/login" class="active">Login</a>
+        <a id="registerLink" href="/register">Register</a>
+    </div>`}
+`;
+
+export function navbarView() {
+    const userData = getUserData();
+    const isUser = userData?.email;
+    render(navbarTemplate(isUser), nav);
+}
+
+function onLogout() {
+    logout();
+    navbarView();
+    page.redirect('/');
+}
