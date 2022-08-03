7d03e1ddb000249e7e28ac17a7a0396a79e58012
NIkolay Koev <nikslotar@abv.bg>
2022-08-04 00:21:29 +0300

    Adde Login and Register

---
+++ JS Applications Exam - 2 April 2022/src/views/navbar.js
@@ -0,0 +1,35 @@
+import { logout } from "../api/users.js";
+import { html, render, page } from "../lib.js";
+import { getUserData } from "../util.js";
+
+const nav = document.querySelector('nav');
+
+const navbarTemplate = (isUser) => html`
+<section class="logo">
+    <img src="./images/logo.png" alt="logo">
+</section>
+<ul>
+    <!--Users and Guest-->
+    <li><a href="/">Home</a></li>
+    <li><a href="#">Dashboard</a></li>
+    ${isUser ? html`
+    <!--Only Guest-->
+    <li><a href="/create">Create Postcard</a></li>
+    <li><a @click=${onLogout} id="logoutBtn" href="javascript:void(0)">Logout</a></li>`
+        : html`
+    <!--Only Users-->
+    <li><a href="/login">Login</a></li>
+    <li><a href="/register">Register</a></li>`}
+</ul>`;
+
+export function navbarView() {
+    const userData = getUserData();
+    const isUser = userData?.email;
+    render(navbarTemplate(isUser), nav);
+}
+
+function onLogout() {
+    logout();
+    navbarView();
+    page.redirect('/');
+}
